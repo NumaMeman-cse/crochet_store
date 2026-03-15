3067e8a11074f1896cf8b06eb14dc77036c07cb3
Numa Meman <memannuma105@gmail.com>
2026-03-16 01:16:12 +0530

    Added database models and setup

--- models.py
+++ models.py
@@ -1,1 +1,38 @@
-m
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(200))
+    address = db.Column(db.Text)
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    description = db.Column(db.Text)
+    price = db.Column(db.Float)
+    image = db.Column(db.String(200))
+    category = db.Column(db.String(100))
+    stock = db.Column(db.Integer)
+
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    total_price = db.Column(db.Float)
+    shipping_price = db.Column(db.Float)
+    payment_status = db.Column(db.String(50))
+    order_status = db.Column(db.String(50))
+
+
+class OrderItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer)
+    product_id = db.Column(db.Integer)
+    quantity = db.Column(db.Integer)
+    price = db.Column(db.Float)
