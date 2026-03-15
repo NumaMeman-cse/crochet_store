d8edd671341b17ab751c19533b4373fb6cec9889
Numa Meman <memannuma105@gmail.com>
2026-03-16 01:24:20 +0530

    Product display system added

--- app.py
+++ app.py
@@ -13,6 +13,34 @@ db.init_app(app)
 def home():
     return render_template('home.html')
 
+from models import Product
+
+@app.route('/products')
+def products():
+
+    all_products = Product.query.all()
+
+    return render_template(
+        "products.html",
+        products=all_products
+    )
+
+@app.route('/add-test-product')
+def add_test_product():
+
+    new_product = Product(
+        name="Crochet Bag",
+        description="Handmade crochet bag",
+        price=499,
+        image="https://via.placeholder.com/300",
+        category="Bags",
+        stock=10
+    )
+
+    db.session.add(new_product)
+    db.session.commit()
+
+    return "Product Added!"
 
 if __name__ == "__main__":
     with app.app_context():
